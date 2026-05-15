1e6e298362bf9e57b3c918462b42ae93c71bfb46
Phạm Thái Hùng - CNTT <hungpt@hyundai.thanhcong.vn>
2026-05-16 02:11:14 +0700

    Initial Commit

---
+++ PoseGuideApp/PoseGuideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseGuideAppApp.swift
+//  PoseGuideApp
+//
+//  Created by Macbook on 16/5/26.
+//
+
+import SwiftUI
+
+@main
+struct PoseGuideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
